1f3231c2b13eb6783a55cbadf60488f1cc698f48
hadiswara <80744322+hadiswara@users.noreply.github.com>
2025-12-12 11:27:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,6 @@ df_pivot = df_filtered.pivot_table(
 )
 
 st.dataframe(df_pivot, use_container_width=True)
-)
 
 # ========== ADDITIONAL ANALYSIS ==========
 st.header("🔬 Analisis Performa KPI Tambahan")
